528755cb4bb06122e55b5d53be9ec58f2bad2412
Hernan Romer <nanug33@gmail.com>
2018-04-17 12:03:52 -0400

    Update runABC.py

--- runABC.py
+++ runABC.py
@@ -1,6 +1,20 @@
-'''Simple script for running the artificial colony, you can select whichever end-value you would like'''
+'''
+Simple sample script to demonstrate how to use the artificial bee colony
+'''
 
 from ABC import *
 
-abc = ABC('''Include whatever parameters you want here''')  # Create an artificial bee colony with an end value of 6
-abc.runABC()  # Run the artificial bee colony
+def fitnessTest(values):  # Fitness function that will be passed to the abc
+    fit = 0
+    for val in values:
+        fit+=val
+    return fit
+  
+values = [('int', (0,100)), ('float', (0,100)), ('int',(0,100))]  # Value type/ranges that will be passed to the abc
+
+abc = ABC(fitnessFunction=fitnessTest, 
+          valueRanges=values, 
+          amountOfEmployers=10, # Defaults to 50
+          iterationAmount=10 # or endValue = 2
+         )
+abc.runABC()
