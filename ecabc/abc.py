ff7e9d85b7f6ea0d28679ca68774a4b11b8c52f5
Hernan Romer <nanug33@gmail.com>
2018-05-01 13:43:53 -0400

    Update abc.py

--- ecabc/abc.py
+++ ecabc/abc.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 #  ecabc/abc.py
-#  v.1.0.3.dev1
+#  v.1.0.4.dev2
 #  Developed in 2018 by Hernan Gelaf-Romer <hernan_gelafromer@student.uml.edu>
 #
 #  This program implements an artificial bee colony to tune ecnet hyperparameters
