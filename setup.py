0afa8483fc23e02b603469c9fbbfec9247bb335a
Hernan Romer <nanug33@gmail.com>
2018-04-30 21:41:12 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name = 'artificial-bee-colony',
+version = "1.0.0.dev1",
+description = 'Artificial bee colony for parameters tuning based on fitness scores',
+url = 'https://github.com/hgromer/Artificial-Bee-Colony',
+author = 'Hernan Gelaf-Romer',
+author_email = 'Hernan_Gelafromer@student.uml.edu',
+license = 'MIT',
+packages = ['abc'],
+install_requires = ["random, numpy,"],
+zip_safe = False)
