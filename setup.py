94bdf98051a83bfa346c59198398c3b836286301
Hernan Romer <nanug33@gmail.com>
2018-05-04 22:45:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-long_description = "Added support for maximizing a fitness function rather than minimizing it through the abc.specifyMinOrMax('max')\
+desc = "Added support for maximizing a fitness function rather than minimizing it through the abc.specifyMinOrMax('max')\
 function. Optimized merging two bees to only look at the bees that are performing well. Overall speed of the program has been increased\
 by around 60%. Added method to allow users to turn off print statements, which may help increase the speed of the code."
 
 setup(name = 'ecabc',
 version = "1.1.0.dev1",
 description = 'Artificial bee colony for parameters tuning based on fitness scores',
-long_description = long_description,
+long_description = desc,
 long_description_content_type = 'text/plain',
 url = 'https://github.com/hgromer/Artificial-Bee-Colony',
 author = 'Hernan Gelaf-Romer',
