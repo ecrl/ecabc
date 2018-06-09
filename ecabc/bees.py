38e91f91795c07f8d850beb1f2b6781cdaf1f4df
Hernan Romer <nanug33@gmail.com>
2018-06-09 16:57:15 -0400

    Updated version

--- ecabc/bees.py
+++ ecabc/bees.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 #  ecabc/bees.py
-#  v.1.1.0.dev1
+#  v.1.1.1.dev2
 #  Developed in 2018 by Hernan Gelaf-Romer <hernan_gelafromer@student.uml.edu>
 #
 #  This program defines the bee objects created in the artificial bee colony
