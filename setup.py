f782beae9748b0db44b4b45e0841f6e187f9e0ab
hgromer <nanug33@gmail.com>
2018-09-04 10:26:44 -0400

    misc

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(name = 'ecabc',
 version = "1.2.0",
 description = 'Artificial bee colony for parameters tuning based on fitness scores',
-long_description = 'settings revamp, bug fixes, algorithm changes',
 long_description_content_type = 'text/plain',
 url = 'https://github.com/ECRL/ecabc',
 author = 'Hernan Gelaf-Romer',
