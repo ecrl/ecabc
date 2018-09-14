35cf31019ea17d8ebb72752a39cd0d18cbf2e298
hgromer <nanug33@gmail.com>
2018-09-14 01:08:43 -0400

    PEP 8 styling change

--- ecabc/settings.py
+++ ecabc/settings.py
@@ -17,18 +17,18 @@ class Settings:
     def __init__(self, valueRanges, num_employers, filename=None):
         self._valueRanges = valueRanges
         self._num_employers = num_employers
-        self._bestValues = []
-        self._bestScore = None
+        self._best_values = []
+        self._best_score = None
         self._minimize = True
 
     ### Save the current settings
     def save_settings(self, filename):
         data = dict()
         data['valueRanges'] = self._valueRanges
-        data['bestValues'] = self._bestValues
+        data['best_values'] = self._best_values
         data['minimize'] = self._minimize
         data['num_employers'] = self._num_employers
-        data['bestScore'] = self._bestScore
+        data['best_score'] = self._best_score
         with open(filename, 'w') as outfile:
             json.dump(data, outfile, indent=4, sort_keys=True)
 
@@ -40,25 +40,25 @@ class Settings:
             with open(filename, 'r') as jsonFile:
                 data = json.load(jsonFile)
                 self._valueRanges = data['valueRanges']
-                self._bestValues = data['bestValues']
+                self._best_values = data['best_values']
                 self._minimize = data['minimize']
                 self._num_employers = data['num_employers']
-                self._bestScore = data['bestScore']
+                self._best_score = data['best_score']
 
     ### Update the current best score/values, returns True if scores were updated
     def update(self, score, values):
         if self._minimize: 
-            if self._bestScore == None or score < self._bestScore:
-                self._bestScore = score
-                self._bestValues = values
+            if self._best_score == None or score < self._best_score:
+                self._best_score = score
+                self._best_values = values
                 return True
         elif not self._minimize:
-            if self._bestScore == None or score > self._bestScore:
-                self._bestScore = score
-                self._bestValues = values
+            if self._best_score == None or score > self._best_score:
+                self._best_score = score
+                self._best_values = values
                 return True
         return False
 
     def get_best(self):
-        return (self._bestScore, self._bestValues)
+        return (self._best_score, self._best_values)
 
