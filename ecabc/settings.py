dce008bee1a7608e99e4a0cdcee4e0cb86ebfb88
Sanskriti Sharma <sanskriti.r.sharma@gmail.com>
2018-09-26 13:57:42 -0400

    Update settings.py

--- ecabc/settings.py
+++ ecabc/settings.py
@@ -3,7 +3,7 @@
 #
 #  ecabc/abc.py
 #  v.2.0.0
-#  Developed in 2018 by Hernan Gelaf-Romer <hernan_gelafromer@student.uml.edu>
+#  Developed in 2018 by Sanskriti Sharma <Sanskriti_Sharma@student.uml.edu> and Hernan Gelaf-Romer <hernan_gelafromer@student.uml.edu>
 #
 #  This class stores the settings for the the bee colony
 #
