430744a7037f4d60d660c029a56dde4de6403770
Hernan <hernan_gelafromer@student.uml.edu>
2018-12-03 17:26:16 -0500

    update sample

--- examples/sample_abc.py
+++ examples/sample_abc.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     while True:
         abc.save_settings('{}/settings.json'.format(os.getcwd()))
         abc.calc_average()
-        if (abc.get_best_performer()[0] < 2):
+        if (getattr(abc, 'best_performer')[0] < 2):
             break
         abc.calc_new_positions()
         abc.check_positions()
