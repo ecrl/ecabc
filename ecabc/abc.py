2396a19c9b6986c1b9db1ccf0b9c78c4ea846785
Sanskriti Sharma <sanskriti.r.sharma@gmail.com>
2018-12-13 13:24:25 -0500

    Added name to code credit -abc.py

--- ecabc/abc.py
+++ ecabc/abc.py
@@ -3,7 +3,7 @@
 #
 #  ecabc/abc.py
 #  v.2.1.0
-#  Developed in 2018 by Hernan Gelaf-Romer <hernan_gelafromer@student.uml.edu>
+#  Developed in 2018 by Sanskriti Sharma <sanskriti_sharma@student.uml.edu> & Hernan Gelaf-Romer <hernan_gelafromer@student.uml.edu>
 #
 #  This program implements an artificial bee colony to tune ecnet hyperparameters
 #
