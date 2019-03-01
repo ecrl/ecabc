5200cc9727145bc9cca872f847b4af9527bc4c2a
Sanskriti Sharma <sanskriti.r.sharma@gmail.com>
2019-03-01 16:25:32 -0500

    Fixed version number - 2.2.0 to 2.2.2

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name = 'ecabc',
-version = "2.1.1",
+version = "2.2.2",
 description = 'Artificial bee colony for parameters tuning based on fitness scores',
 url = 'https://github.com/ECRL/ecabc',
-author = 'Hernan Gelaf-Romer',
-author_email = 'Hernan_Gelafromer@student.uml.edu',
+author = 'Sanskriti Sharma, Hernan Gelaf-Romer',
+author_email = 'Sanskriti_Sharma@student.uml.edu, Hernan_Gelafromer@student.uml.edu',
 license = 'MIT',
 packages = ['ecabc'],
 install_requires = ["numpy", "ColorLogging"],
